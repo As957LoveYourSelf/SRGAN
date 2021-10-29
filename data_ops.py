6baa5fe1b7a94fa49111090a601fc0ea08ca0c95
As957LoveYourSelf <1290450579@qq.com>
2021-10-29 09:07:53 +0800

    update data_ops.py

--- data_ops.py
+++ data_ops.py
@@ -2,7 +2,6 @@ import torchvision
 import torch
 from torch import Tensor
 import torchvision.transforms as transforms
-from torch.utils.data.dataset import T_co
 from torchvision.utils import save_image
 import torch.nn.functional as F
 import glob
@@ -11,30 +10,31 @@ import tqdm
 import cv2
 from torch.utils.data import Dataset
 
-HR_image_path = "/dataset/HR"
-train_lr_image_path = "/dataset/train/LR"
-train_hr_image_path = "/dataset/train/HR"
-test_lr_image_path = "/dataset/test/LR"
-test_hr_image_path = "/dataset/test/HR"
+HR_image_path = "dataset/HR"
+train_lr_image_path = "dataset/train/LR"
+train_hr_image_path = "dataset/train/HR"
+test_lr_image_path = "dataset/test/LR"
+test_hr_image_path = "dataset/test/HR"
 
 if not os.path.exists(train_hr_image_path):
-    os.mkdir(test_hr_image_path)
-    os.mkdir(test_lr_image_path)
-    os.mkdir(train_lr_image_path)
-    os.mkdir(train_hr_image_path)
+    os.makedirs(test_hr_image_path)
+    os.makedirs(test_lr_image_path)
+    os.makedirs(train_lr_image_path)
+    os.makedirs(train_hr_image_path)
 
 tran = transforms.Compose(
     [
         transforms.ToTensor(),
-        transforms.RandomCrop(96),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.RandomCrop(96)
     ]
 )
 
 
 def hr2lr(tensor: Tensor, scale_factor):
     assert (isinstance(tensor, Tensor))
-    return F.interpolate(tensor, scale_factor=scale_factor, mode="bilinear")
+    t_shape = tensor.shape
+    tensor = tensor.view((1, *list(t_shape)))
+    return F.interpolate(tensor, scale_factor=scale_factor)
 
 
 def save_hr2lr(hr_image, save_hr_path, save_lr_path):
@@ -49,32 +49,41 @@ def save_hr2lr(hr_image, save_hr_path, save_lr_path):
 
 
 def make_dateset(hr_image_path):
-    hr_images = glob.glob(hr_image_path + "/*")
+    hr_images = glob.glob(hr_image_path+"/*")
     l = len(hr_images)
-    train_len = l*0.8
+    train_len = int(l * 0.8)
     print(f"Begin Build Train DataSet, Length = {train_len}")
     for hr_image in tqdm.tqdm(hr_images[:train_len]):
-        save_hr2lr(hr_image, train_hr_image_path, train_lr_image_path)
-    print(f"Begin Build Test DataSet, Length = {l-train_len}")
+        try:
+            save_hr2lr(hr_image, train_hr_image_path, train_lr_image_path)
+        except ValueError:
+            continue
+    print(f"Begin Build Test DataSet, Length = {l - train_len}")
     for hr_image in tqdm.tqdm(hr_images[train_len:]):
-        save_hr2lr(hr_image, test_hr_image_path, test_lr_image_path)
+        try:
+            save_hr2lr(hr_image, test_hr_image_path, test_lr_image_path)
+        except ValueError:
+            continue
 
 
 class PreDateSet(Dataset):
-    def __init__(self):
+    def __init__(self, HR_path, LR_path):
         super(PreDateSet, self).__init__()
+        hr_images = glob.glob(os.path.join(HR_path, "*"))
+        lr_images = glob.glob(os.path.join(LR_path, "*"))
+        self.images_pair_data = list(zip(hr_images, lr_images))
 
-    def __getitem__(self, index) -> T_co:
-        pass
+    def __getitem__(self, index):
+        tran_ = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        hr_img, lr_img = self.images_pair_data[index]
+        hr_img, lr_img = tran_(cv2.imread(hr_img)), tran_(cv2.imread(lr_img))
+        return hr_img, lr_img
 
     def __len__(self):
-        pass
+        return len(self.images_pair_data)
 
 
-def main():
-    pass
-
-
-
-if __name__ == '__main__':
-    main()
+make_dateset(HR_image_path)
