5d3338d45723bd8189abeb3c022a74222ddb66fe
As957LoveYourSelf <1290450579@qq.com>
2021-10-30 16:55:24 +0800

    update files

--- train.py
+++ train.py
@@ -25,7 +25,9 @@ for p in vggnet.parameters():
 
 # load model
 g_net = Generator(residual_num=19, upsample_x=2)
+g_net.to(device)
 d_net = Discriminator()
+d_net.to(device)
 print("G_net model summary:")
 summary.summary(g_net, input_size=(3, 24, 24))
 print("D_net model summary:")
