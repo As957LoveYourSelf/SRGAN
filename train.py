a70d129b3aa7fd5b99182d56fe8cb73ae6230b86
As957LoveYourSelf <1290450579@qq.com>
2021-10-31 18:26:12 +0800

    update files

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+import tqdm
 from torchvision.utils import save_image
 from net import Generator, Discriminator
 import torch
@@ -9,7 +12,7 @@ import loss_functions as losses
 from torch.optim import Adam, SGD
 from tensorboardX import SummaryWriter
 
-logger = SummaryWriter(log_dir="result/log")
+# logger = SummaryWriter(log_dir="result/log")
 
 vggnet = vgg19()
 pretrainmodel_path = "pretrainmodels/vgg19.pth"
@@ -42,8 +45,8 @@ if not os.path.exists(save_image_path):
 # define parameters
 EPOCH = 10000
 BATCH_SIZE = 8
-lr_g = 1e-4
-lr_d = 1e-4
+lr_g = 1e-5
+lr_d = 1e-5
 
 optimizer_G = Adam(g_net.parameters(), lr=lr_g)
 optimizer_D = Adam(d_net.parameters(), lr=lr_d)
@@ -54,11 +57,10 @@ train_data = DataLoader(train_predataset, batch_size=BATCH_SIZE, shuffle=True)
 test_predataset = PreDateSet(test_hr_image_path, test_lr_image_path)
 test_data = DataLoader(test_predataset, batch_size=BATCH_SIZE, shuffle=False)
 test_data = iter(test_data)
-train_data_l = len(train_predataset)
-test_data_l = len(test_predataset)
+train_data_l = len(train_data)
 
-for epoch in range(EPOCH):
-    for i, (hr_image, lr_image) in enumerate(train_data):
+for epoch in range(1, EPOCH+1):
+    for i, (hr_image, lr_image) in tqdm.tqdm(enumerate(train_data, start=1)):
 
         d_net.train()
         g_net.train()
@@ -71,12 +73,11 @@ for epoch in range(EPOCH):
         g_hr_img = g_net(lr_image)
         disc_g_img = d_net(g_hr_img)
         # 3 parts losses
-        content_loss = losses.mse_loss(hr_image, g_hr_img)
-        vgg_loss = losses.vgg_loss(vggnet(hr_image), vggnet(g_hr_img))
-        adv_loss = losses.adv_loss(disc_g_img)
+        content_loss = losses.mse_loss(hr_image, g_hr_img).mean()
+        vgg_loss = losses.vgg_loss(vggnet(hr_image), vggnet(g_hr_img)).mean()
+        adv_loss = losses.adv_loss(disc_g_img).mean()
 
         gen_loss = content_loss + 1e-3*vgg_loss + 1e-6*adv_loss
-
         gen_loss.backward()
         optimizer_G.step()
 
@@ -85,32 +86,31 @@ for epoch in range(EPOCH):
         #######################
         optimizer_D.zero_grad()
 
-        real = torch.ones_like(disc_g_img, requires_grad=False)
-
         real_d_img = d_net(hr_image)
-        disc_loss = real - disc_g_img + real_d_img
+        disc_loss = 1. - disc_g_img.mean().detach() + real_d_img.mean()
         disc_loss.backward()
         optimizer_D.step()
 
         # print train status
-        print(f"[{epoch}/{EPOCH}Epoch]=>G_loss: {gen_loss.item}|D_predict: {disc_loss.item}|[{i}/{train_data_l}]Iter")
+        print(f"[{epoch}/{EPOCH}Epoch][{i}/{train_data_l}Iter]=>G_loss: {gen_loss.item()}|D_predict: "
+              f"{disc_loss.item()}")
 
         # save result (images)
-        g_net.eval()
-        d_net.eval()
-        if i % 100 == 0:
+        if i % 2000 == 0:
+            g_net.eval()
+            d_net.eval()
             hr_img, lr_img = next(test_data)
-            with torch.no_grad:
+            with torch.no_grad():
                 fake_hr_img = g_net(lr_img)
                 predict_img = d_net(fake_hr_img)
             res = torch.cat([fake_hr_img, hr_img], dim=0)
             res = res.cpu()
             print("Begin save image...")
-            save_image(fake_hr_img, save_image_path+f"{epoch}_epoch"+f"_{i}_iter_predict_{d_net}"+".jpg")
+            save_image(res, os.path.join(save_image_path, f"{epoch}_epoch_{i}_iter.jpg"))
             print("save success.")
-        gen_model_name = f"{epoch}_epoch_gen_model.pth"
-        disc_model_name = f"{epoch}_epoch_disc_model.pth"
-        print("Saving generate model...")
-        torch.save(g_net.state_dict(), gen_model_name)
-        print("Saving discriminate model...")
-        torch.save(d_net.state_dict(), disc_model_name)
+    gen_model_name = f"{epoch}_epoch_gen_model.pth"
+    disc_model_name = f"{epoch}_epoch_disc_model.pth"
+    print("Saving generate model...")
+    torch.save(g_net.state_dict(), os.path.join(save_model_path, gen_model_name))
+    print("Saving discriminate model...")
+    torch.save(d_net.state_dict(), os.path.join(save_model_path, disc_model_name))
